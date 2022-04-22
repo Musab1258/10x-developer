a983877239f94f90e41b9556af6732efaffcaac6
Habeeb Musab <habeebmusab@gmail.com>
2022-04-22 09:34:40 +0100

    worked on the homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Nav from './components/Nav';
+import HardTime from './images/hard-time.png'
+import ProgrammingTools from './images/programming-tools.png'
+import MentorGuide from './images/mentor-guide.png'
+
 
 function App() {
   return (
@@ -7,12 +11,26 @@ function App() {
       <header>
         <Nav />
       </header>
-      <p>Learning software development can be hard at the beginning</p>
-      <p>There are a lot of tools and technologies and it seems you can't wrap your head around them</p>
-      <p>Sometimes all what you need is someone to guide you.</p>
-      <p>If this is you, then congrats. You have come to the right place</p>
-      <p>I have over one year of experience in Frontend Development and I have got friends in other areas of software development that can help out.</p>
-      <p>All what you need to do is to schedule an appointment. And don't worry it is totally free.</p>
+      <body className='m-5'>
+        <div className='m-16 p-5 text-xl border-2 rounded shadow-lg'>
+          <p>Learning software development can be hard at the beginning</p>
+          <img src={HardTime} />
+        </div>
+        <div className='m-16 p-5 text-xl border-2 rounded shadow-lg'>
+          <p>There are a lot of tools and technologies to learn</p>
+          <img src={ProgrammingTools} />
+        </div>
+        <div className='m-16 p-5 text-xl border-2 rounded shadow-lg'>
+          <p>And sometimes, you need an experienced Developer to guide you.</p>
+          <img src={MentorGuide} />
+        </div>
+        <div className='m-16 p-5 text-xl border-2 rounded shadow-lg'>
+          <p className='my-5'>I am an experienced Frontend Developer and I have got friends in other areas of software development that can help out.</p>
+          <p className='my-5'>All what you need to do is to schedule an appointment. And don't worry it is totally free.</p>
+          <button className='border border-black my-5'>Schedule</button>
+        </div>
+        
+      </body>
     </div>
   );
 }
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Nav from './Nav';
+
+
 const Schedule = () => {
-    return  (
-        <h1>Schedule</h1>
-    )
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Nav />
+      <Calendar className="m-auto" onChange={onChange} value={value} />
+    </div>
+  );
 }
 
 export default Schedule;
