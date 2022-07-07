66b22457d7b3deb73d00ec2bba563a4ffe05a6c6
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-07-07 03:27:22 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
               <div className="flex-shrink-0">
                 <img
                   className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                  src=""
                   alt="Workflow"
                 />
               </div>
