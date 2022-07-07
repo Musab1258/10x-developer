2d685348a3bfcb4c383d7b3f8ed79b07f27bc913
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-07-07 03:38:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ function App() {
         </div>
         
         <div className='grid md:grid-cols-2'>
-        <div className='m-16 p-5 text-xl border-2 rounded shadow-lg'>
+        <div className='my-16 mx-5 p-5 grid md:grid-cols-2 text-xl border-2 rounded shadow-lg'>
           <p className='my-5'>I am an experienced Frontend Developer and I have got friends in other areas of software development that can help out.</p>
         </div>
-        <div className='m-3 p-5 md:col-span-2 md:w-6/12 text-xl border-2 rounded shadow-lg'>
+        <div className='my-16 mx-5 p-5 grid md:grid-cols-2 text-xl border-2 rounded shadow-lg'>
           <p className='my-5'>All what you need to do is to schedule an appointment. And don't worry it is totally free.</p>
           <button className='border border-black py-2 px-4 rounded shadow-lg'>Schedule</button>
         </div>
